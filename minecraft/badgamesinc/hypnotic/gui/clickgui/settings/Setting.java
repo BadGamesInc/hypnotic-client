a13cb7dd40fb05c39c8226b15f671d66808d20db
BadGamesInc <75589827+BadGamesInc@users.noreply.github.com>
2021-04-12 12:58:30 -0700

    Might fix configs, cant test because im writing this in my social studies class lmao

--- minecraft/badgamesinc/hypnotic/gui/clickgui/settings/Setting.java
+++ minecraft/badgamesinc/hypnotic/gui/clickgui/settings/Setting.java
@@ -36,6 +36,10 @@ public class Setting {
         this.sval = sval;
         this.options = options;
         this.mode = "Combo";
+	if (Hypnotic.instance.saveload != null) {
+            sval = this.getValString
+        }
+	    //probably doesnt work, will fix when i get home
     }
 
     public Setting(String name, Mod parent, boolean bval) {
@@ -43,6 +47,10 @@ public class Setting {
         this.parent = parent;
         this.bval = bval;
         this.mode = "Check";
+	    if (Hypnotic.instance.saveload != null) {
+            bval = this.getValBoolean
+        }
+	    //probably doesnt work, will fix when i get home
     }
 
     public Setting(String name, Mod parent, double dval, double min, double max, boolean onlyint) {
@@ -53,6 +61,10 @@ public class Setting {
         this.max = max;
         this.onlyint = onlyint;
         this.mode = "Slider";
+	    if (Hypnotic.instance.saveload != null) {
+            dval = this.getValDouble
+        }
+	    //probably doesnt work, will fix when i get home
     }
 
     public String getName() {
