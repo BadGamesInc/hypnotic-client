41c9b91d0e2cd2d659e958fbba832c597cc53eb7
BadGamesInc <75589827+BadGamesInc@users.noreply.github.com>
2021-04-15 12:02:31 -0700

    Create Seed.java

---
+++ minecraft/badgamesinc/hypnotic/command/commands/Seed.java
@@ -0,0 +1,34 @@
+package badgamesinc.hypnotic.command.commands;
+
+import badgamesinc.hypnotic.command.Command;
+import badgamesinc.hypnotic.util.Wrapper;
+import net.minecraft.client.multiplayer.GuiConnecting;
+import net.minecraft.client.multiplayer.ServerAddress;
+import net.minecraft.network.play.client.C01PacketChatMessage;
+
+public class Server extends Command {
+
+	@Override
+	public String getAlias() {
+		
+		return "seed";
+	}
+
+	@Override
+	public String getDescription() {
+		
+		return "The seed of the current world";
+	}
+
+	@Override
+	public String getSyntax() {
+		
+		return ".seed";
+	}
+
+	@Override
+	public void onCommand(String command, String[] args) throws Exception 
+	{
+		Wrapper.tellPlayer(mc.theWorld.getSeed() + "");
+	}
+}
