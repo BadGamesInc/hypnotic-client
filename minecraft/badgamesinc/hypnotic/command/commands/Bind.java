b3f18884239ce57f9f1cda77ea448d05dda6dde3
BadGamesInc <ibadgames5000@gmail.com>
2021-05-22 18:47:06 -0700

    No I FIXED THE KEYBINDS sDKJFH ksdjhfkjshfkjdshfkjdshkfj

--- minecraft/badgamesinc/hypnotic/command/commands/Bind.java
+++ minecraft/badgamesinc/hypnotic/command/commands/Bind.java
@@ -7,6 +7,7 @@ import badgamesinc.hypnotic.gui.notifications.Notification;
 import badgamesinc.hypnotic.gui.notifications.NotificationType;
 import badgamesinc.hypnotic.module.Mod;
 import badgamesinc.hypnotic.util.ColorUtils;
+import badgamesinc.hypnotic.util.SoundUtil;
 import badgamesinc.hypnotic.util.Wrapper;
 
 public class Bind extends Command {
@@ -30,29 +31,45 @@ public class Bind extends Command {
 	}
 
 	@Override
-	public void onCommand(String command, String[] args) throws Exception 
-	{
-		if(args[0].equalsIgnoreCase("clear")) 
-		{
-			if(args[1].equalsIgnoreCase("all")) 
-			{
-				for(Mod m : Hypnotic.instance.moduleManager.modules) 
-				{
+	public void onCommand(String command, String[] args) throws Exception {
+		SoundUtil.playSound("on.wav");
+		if (args.length == 2) {
+				String moduleName = args[0];
+				String keyName = args[1];
+
+			for(Mod m : Hypnotic.instance.instance.moduleManager.modules) {
+				m.getName().replaceAll(" ", "");
+				if(m.getName().equalsIgnoreCase(moduleName)) {
+					if (args[1].length() == 1) {
+						m.setKey(Keyboard.getKeyIndex(keyName.toUpperCase()));
+	
+						Hypnotic.instance.notificationManager.show(new Notification(String.format("Bound %s to %s", ColorUtils.white + m.getName() + ColorUtils.gray, ColorUtils.white + Keyboard.getKeyName(m.getKey())) + "     ", 5, NotificationType.INFO));
+						if (args[1].length() != 1) {
+						Hypnotic.instance.notificationManager.show(new Notification(String.format(args[1]), 5, NotificationType.INFO));
+						
+						}
+					} else if (args[1].equalsIgnoreCase("none")){
+						m.setKey(Keyboard.KEY_NONE);
+						Hypnotic.instance.notificationManager.show(new Notification("Unbound " + m.getName(), 5, NotificationType.INFO));
+					}
+				}
+			}
+		}
+
+		if(args.length == 1) {
+			if(args[0].equalsIgnoreCase("clear")) {
+				for(Mod m : Hypnotic.instance.moduleManager.modules) {
 					m.setKey(0);
 				}
-				Hypnotic.instance.notificationManager.show(new Notification(ColorUtils.white + "Unbound all modules", (int) 5, NotificationType.INFO));
-			}
-			else if(!args[1].equalsIgnoreCase("all"))
-			{
-				Hypnotic.instance.moduleManager.getModuleByName(args[1]).setKey(0);
-				Hypnotic.instance.notificationManager.show(new Notification(ColorUtils.white + "Unbound " + ColorUtils.gray + args[1], (int) 5, NotificationType.INFO));
-			}
+				Wrapper.tellPlayer("Cleared all binds");
+			} else 
+				Wrapper.tellPlayer(args[0] + " is not a module");
 		}
-		else 
-		{
-			Hypnotic.instance.moduleManager.getModuleByName(args[1]).setKey(Keyboard.getKeyIndex(args[0].toUpperCase()));
-			Hypnotic.instance.notificationManager.show(new Notification(ColorUtils.white + "Bound " + ColorUtils.gray + args[1] + ColorUtils.white + " to " + ColorUtils.gray + args[0], (int) 5, NotificationType.INFO));
+
+		if(args[0] == null) {
+			Wrapper.tellPlayer("Usage: " + getSyntax());
 		}
+
 	}
 
 }
